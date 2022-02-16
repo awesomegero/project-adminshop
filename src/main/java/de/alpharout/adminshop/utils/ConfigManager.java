86d312d882ec79822977193132b76117636dbbb4
alphaR0ut <gero.lindner@protonmail.com>
2022-02-16 18:27:17 +0100

    Added messages.yml

--- src/main/java/de/alpharout/adminshop/utils/ConfigManager.java
+++ src/main/java/de/alpharout/adminshop/utils/ConfigManager.java
@@ -1,12 +1,18 @@
 package de.alpharout.adminshop.utils;
 
 import de.alpharout.adminshop.AdminShop;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ConfigManager {
     private File databaseFile;
     private File messagesFile;
+    private YamlConfiguration databaseConf;
+    private YamlConfiguration messagesConf;
 
     public ConfigManager() {
 
@@ -23,6 +29,25 @@ public class ConfigManager {
         if (!messagesFile.exists()) {
             AdminShop.getInstance().saveResource("messages.yml", false);
             Log.debug("Created messages.yml file.");
+        }
+
+        String currentLoadingConfig = "unknown";
+        try {
+            databaseConf = new YamlConfiguration();
+            messagesConf = new YamlConfiguration();
+
+            currentLoadingConfig = databaseFile.getName();
+            databaseConf.load(databaseFile);
+
+            currentLoadingConfig = messagesFile.getName();
+            messagesConf.load(messagesFile);
+        } catch (FileNotFoundException e) {
+            Log.critical("Configuration file for " + currentLoadingConfig + " hasn't been found!");
+            return;
+        } catch (IOException e) {
+            Log.critical("Error while reading " + currentLoadingConfig + "!");
+        } catch (InvalidConfigurationException e) {
+            Log.critical("Invalid YAML code in " + currentLoadingConfig + "!");
         }
     }
 }
